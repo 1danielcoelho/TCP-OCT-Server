afb57590682c86f2577bcde7fb3321c7a6befb30
Daniel Coelho <1danielcoelho@gmail.com>
2014-07-17 21:20:55 +0200

    Updated to-do list

--- TCP Testing/main.cpp
+++ TCP Testing/main.cpp
@@ -2,28 +2,24 @@
 To-do list:
 
 >> Things to do at the lab
-	-Testing
-		+Check to see if there is any code in the demo that renders 4d plots
-	-Qtcreator and ROS
-		+Figure out how to run the OCT_Wrapper node and how to call its functions and services
-		+Find out what swap and resize do
-			They are methods of imageMessage.data, inside publishImageDataFromDynamicReconfigure.
-			I could pass directly the data pointer from m_client if swap copies by value. If it's some kind of reference then that's not doable since I can't guarantee m_fileData stays alive forever		
-	-Client - Server connection
-		+Transfer a block with all params before capture to prevent spam when changing sliders on dynamic reconfigure
-	-Dynamic reconfigure
-		+Add groups to the cfg file
-		+Lidar com o Z range depender do Z steps
-		+Complete publishImageDataFromDynamicReconfigure
-		+Add octType in the interface exactly like the publish function (like a param pre-set)
-	-Service, requests and responses
-		+Are all three services needed? We could pass the octType or the other params through the message
+-Testing
++Check to see if there is any code in the demo that renders 4d plots
+-Qtcreator and ROS
++Figure out how to run the OCT_Wrapper node and how to call its functions and services
+
+-Dynamic reconfigure
++Add groups to the cfg file
++Lidar com o Z range depender do Z steps
++Complete publishImageDataFromDynamicReconfigure
++Add octType in the interface exactly like the publish function (like a param pre-set)
+-Service, requests and responses
++Are all three services needed? We could pass the octType or the other params through the message
 
 >> Improvements
-	-Find a way of looking at the data produced by a volume scan
-	-Have the client try and reconnect after a connection has been dropped for some reason
-	-Find out the networks speed at the lab and consider transferring the camera output
-		+JPEG compression?
+-Find a way of looking at the data produced by a volume scan
+-Have the client try and reconnect after a connection has been dropped for some reason
+-Find out the networks speed at the lab and consider transferring the camera output
++JPEG compression?
 
 */
 
@@ -33,7 +29,7 @@ To-do list:
 #include "boost/asio.hpp"
 #include <boost/asio.hpp>
 
-#include <Dummy SDOCT.h>
+#include <SDOCT.h>
 #include <TCP_Server.h>
 
 #include <iostream>
@@ -41,57 +37,63 @@ To-do list:
 #include <sstream>
 #include <string>
 
-//int main()
-//{
-//	try
-//	{
-//		boost::asio::io_service service;
-//		SDOCT oct;
-//
-//		TCP_Server server(service, oct);
-//
-//		service.run();
-//
-//
-//	}
-//	catch (...)
-//	{
-//		std::cout << "main exception" << std::endl;
-//	}
-//
-//	return 0;
-//}
+int main()
+{
+	try
+	{
+		boost::asio::io_service service;
+		SDOCT oct;
+
+		TCP_Server server(service, oct);
+
+		service.run();
+
+
+	}
+	catch (...)
+	{
+		std::cout << "main exception" << std::endl;
+	}
+
+	return 0;
+}
 //
 ////OCT_Wrapper: msg, srv, cfg, OCT_wrapper_qtros.hpp and .cpp
 ////OCT_Segmentation: msg, srv, OCT_segmentation_qtros.cpp
 //
 //
 
-int main()
-{
-	SDOCT oct;
-	oct.Init();
-
-	std::vector<uint8_t> voxeldata;
-	voxeldata.reserve(4096);
-	oct.captureVolScan(voxeldata);
-
-	std::ofstream m_fileStream;
-
-	std::string path = "C:\\Users\\1dani_000\\Desktop\\voxeldata.txt";
-
-	m_fileStream.open(path.c_str(), std::ios::binary | std::ios::out);
-	if (!m_fileStream)
-	{
-		std::cout << "Couldn't create the file!" << std::endl;
-	}
-
-	std::string number;
-
-	for (int i = 0; i < 4096; i++)
-	{
-		number = boost::lexical_cast<std::string>((int)((unsigned char)voxeldata[i])) + " ";
-		if (m_fileStream.is_open()) m_fileStream.write(number.c_str(), number.size());
-	}
-
-}
+//int main()
+//{
+//    SDOCT oct;
+//    oct.Init();
+// 
+//    std::vector<uint8_t> voxeldata;
+//    voxeldata.reserve(64*64*1024);
+//    oct.captureVolScan(voxeldata);
+// 
+//    std::ofstream m_fileStream;
+// 
+//    std::string path = "C:\\Users\\OCT\\Desktop\\voxeldata.txt";
+// 
+//    m_fileStream.open(path.c_str(), std::ios::binary | std::ios::out);
+//    if (!m_fileStream)
+//    {
+//        std::cout << "Couldn't create the file!" << std::endl;
+//    }
+// 
+//    std::string number;
+// 
+//    for (int i = 0; i < 64*64*1024; i++)
+//    {
+//        number = boost::lexical_cast<std::string>((int)((unsigned char)voxeldata[i])) + " ";
+//        
+////
+//		if((i+1)%1024 == 0) number += "line\n ";
+//		if((i+1)%(1024*64) == 0) number += "bscan\n\n\n ";
+////
+//		if (m_fileStream.is_open()) m_fileStream.write(number.c_str(), number.size());
+//		
+//    }
+// 
+//}
