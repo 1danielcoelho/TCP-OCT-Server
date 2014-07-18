efa4e465e96134ab86fc965f8e8b114e62de66ce
1danielcoelho <1danielcoelho@gmail.com>
2014-07-18 16:24:34 +0200

    Update main.cpp

--- TCP Testing/main.cpp
+++ TCP Testing/main.cpp
@@ -1,67 +1,68 @@
 /*
 To-do list:
-
+ 
 >> Things to do at the lab
-	-Visualization
-		+Fix the output from the SDOCT class
-			Use the demo code for example
-			Is it a fourier transform of the data?
-	-TCP-IP
-		+Finish implementing 100 byte edge tests for headers and voxel data
-			Stopped responding for no reason
-			Debug afc_test_client to see header pointer
-		+Have the server clean up after a connection a bit better
-		+Decide when to initialize/close
-	-Dynamic reconfigure
-		+Add groups to the cfg file
-		+Deal with how Z range depends on Z steps
-	
-	
+    -Testing
+        +Check to see if there is any code in the demo that renders 4d plots
+    -Qtcreator and ROS
+        +Figure out how to run the OCT_Wrapper node and how to call its functions and services
+        
+    -Dynamic reconfigure
+        +Add groups to the cfg file
+        +Lidar com o Z range depender do Z steps
+        +Complete publishImageDataFromDynamicReconfigure
+        +Add octType in the interface exactly like the publish function (like a param pre-set)
+    -Service, requests and responses
+        +Are all three services needed? We could pass the octType or the other params through the message
+ 
 >> Improvements
-	
-	
+    -Find a way of looking at the data produced by a volume scan
+    -Have the client try and reconnect after a connection has been dropped for some reason
+    -Find out the networks speed at the lab and consider transferring the camera output
+        +JPEG compression?
+ 
 */
-
+ 
 //#define BOOST_ASIO_ENABLE_HANDLER_TRACKING
-
-
+ 
+ 
 #include "boost/asio.hpp"
 #include <boost/asio.hpp>
-
+ 
 #include <SDOCT.h>
 #include <TCP_Server.h>
-
+ 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
-
+ 
 int main()
 {
-	try
-	{
-		boost::asio::io_service service;
-		SDOCT oct;
+  try
+  {
+      boost::asio::io_service service;
+      SDOCT oct;
 
-		TCP_Server server(service, oct);
+      TCP_Server server(service, oct);
 
-		service.run();
+      service.run();
 
 
-	}
-	catch (...)
-	{
-		std::cout << "main exception" << std::endl;
-	}
+  }
+  catch (...)
+  {
+      std::cout << "main exception" << std::endl;
+  }
 
-	return 0;
+  return 0;
 }
 //
 ////OCT_Wrapper: msg, srv, cfg, OCT_wrapper_qtros.hpp and .cpp
 ////OCT_Segmentation: msg, srv, OCT_segmentation_qtros.cpp
 //
 //
-
+ 
 //int main()
 //{
 //    SDOCT oct;
